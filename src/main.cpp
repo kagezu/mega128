82159d0655e61b00811d7596c5b8ce3b3dbf0b47
hinto <hinto@rambler.ru>
2024-10-25 21:52:41 +0700

    add: шрифт 5х6

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "ST7735S.h"
+#include "font_6x5.h"
 
 void error()
 {
@@ -10,8 +11,39 @@ void error()
 
 ST7735S lcd;
 
+void setSymbol(byte x, byte y, byte symbol)
+{
+  // lcd.setBlock(x, y, x + 5, y + 6);
+  for (byte i = 0; i < 6; i++)
+  {
+    byte data = pgm_read_byte(font_6x5 + symbol * 6 + i);
+    byte v = 0x10;
+    byte j = 0;
+    while (v)
+    {
+      word color = data & v ? 0xfff : 0x500;
+      // lcd.data_12(color);
+      lcd.pixel(x + 5 - j, y + i, color);
+      v = v >> 1;
+      j++;
+    }
+  }
+
+  SET_CS // CS Снять выбор дисплея
+}
+
 int main(void)
 {
+  lcd.clear(0x500);
+  byte n = 30;
+  for (byte y = 0; y < 150; y += 7)
+
+    for (byte x = 0; x < 120; x += 6)
+    {
+      setSymbol(x, y, n++);
+    }
+
+  /*
   int x = 1;
   while (true)
   {
@@ -20,8 +52,6 @@ int main(void)
     //  lcd.clear(0x0f0);
     //  lcd.clear(0xf00);
   }
-
-  /*
     PORTF = 0;
     DDRF = 0xFF;
 
@@ -48,6 +78,4 @@ int main(void)
       PORTF += 1;
     }
   */
-
-  // return 0;
 }
