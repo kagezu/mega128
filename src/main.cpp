38c6cf1efa56dcb0968732357c6e138953f37b3c
hinto <hinto@rambler.ru>
2024-10-25 07:06:11 +0700

    add: lcd display lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-// #define F_CPU 14745600L
 #include <Arduino.h>
+#include "ST7735S.h"
 
 void error()
 {
@@ -8,34 +8,52 @@ void error()
     ;
 }
 
+ST7735S lcd;
+
 int main(void)
 {
 
-  PORTF = 0;
-  DDRF = 0xFF;
+  delayMicroseconds(55000);
 
-  PORTE &= ~_BV(PE6); // Set 0 to A16
-  DDRE |= _BV(PE6);   // Output A16
+  lcd.init();
+  lcd.clear(0);
 
-  MCUCR |= _BV(SRE); // XRAM Enable
-
+  int x = 1;
   while (true)
   {
-    uint8_t __volatile__ *address = (uint8_t *)0x1100;
-    while (address++)
-    {
-      *address = 0;
-      if (*address)
-        error();
-
-      *address = 0xff;
-      if (*address != 0xff)
-        error();
-    }
-
-    // delay(100);
-    PORTF += 1;
+    lcd.test(x++);
+    //  lcd.clear(0x00f);
+    //  lcd.clear(0x0f0);
+    //  lcd.clear(0xf00);
   }
 
-  return 0;
+  /*
+    PORTF = 0;
+    DDRF = 0xFF;
+
+    PORTE &= ~_BV(PE6); // Set 0 to A16
+    DDRE |= _BV(PE6);   // Output A16
+
+    MCUCR |= _BV(SRE); // ESRAM Enable
+
+    while (true)
+    {
+      uint8_t __volatile__ *address = (uint8_t *)0x1100;
+      while (address++)
+      {
+        *address = 0;
+        if (*address)
+          error();
+
+        *address = 0xff;
+        if (*address != 0xff)
+          error();
+      }
+
+      // delay(100);
+      PORTF += 1;
+    }
+  */
+
+  // return 0;
 }
