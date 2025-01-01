2ba016fc2b97d2ad4a9a817056894cbd128285a9
hinto <hinto@rambler.ru>
2025-01-02 05:55:56 +0700

    fix: MemoryBlock class

--- lib/memory/src/memory/memory-block.h
+++ lib/memory/src/memory/memory-block.h
@@ -1,41 +1,50 @@
 #include <Arduino.h>
 
-#define MEM_USED    128
-#define MEM_FIXED   64
-#define MEM_LOCK    32
-#define MEM_FREE    0
+#define MEM_USED      128
+#define MEM_FIXED     64
+#define MEM_LOCK      32
+#define MEM_FREE      0
 
 class MemoryBlock {
 protected:
-  uint16_t _link_to_index;
+  uint16_t _link_to_ptr;
   uint16_t _start;
   uint16_t _size;
   volatile byte _status;
-  byte _reserve;
+  // byte _reserve;
 
 public:
   MemoryBlock() {}
   void init(uint16_t start, uint16_t size = 0)
   {
-    _link_to_index = 0;
     _start = start;
     _size = size;
     _status = MEM_FREE;
   }
 
 public:
-  inline void free() { *(uint16_t *)_link_to_index = 0; _link_to_index = 0; }
+  void used() { _status = MEM_USED | MEM_LOCK; }
+  void used(uint16_t link_to_ptr)
+  {
+    _link_to_ptr = link_to_ptr;
+    *(uint16_t *)link_to_ptr = _start;
+    _status = MEM_USED;
+  }
+  void free()
+  {
+    if (!(_status & MEM_FIXED)) *(uint16_t *)_link_to_ptr = 0;
+    _status = MEM_FREE;
+  }
 
-  inline void set_link(uint16_t linkToIndex) { _link_to_index = linkToIndex; *(uint16_t *)linkToIndex = _start; }
-  inline void set_size(uint16_t size) { _size = size; }
-  inline void set_start(uint16_t start) { _start = start; }
+  void set_start(uint16_t start) { _start = start; }
+  void set_size(uint16_t size) { _size = size; }
 
-  inline void lock() { _status |= MEM_LOCK; }
-  inline void unlock() { _status &= ~MEM_LOCK; }
-  inline void fixed() { _status |= MEM_FIXED; }
+  uint16_t get_link() { return _link_to_ptr; }
+  uint16_t get_start() { return _start; }
+  uint16_t get_size() { return _size; }
 
-  inline uint16_t getLink() { return _link_to_index; }
-  inline uint16_t getSize() { return _size; }
-  inline uint16_t getStart() { return _start; }
-  inline byte getStatus() { return _status; }
+  bool is_locked() { return _status & (MEM_LOCK | MEM_FIXED); }
+  void lock() { _status |= MEM_LOCK; }
+  void unlock() { _status &= ~MEM_LOCK; }
+  void fixed() { _status |= MEM_FIXED; }
 };
