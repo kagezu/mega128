e661d687ad69730770b22baba8e00319d3f91e99
hinto <hinto@rambler.ru>
2025-01-02 06:25:01 +0700

    fix: MemoryBlock class

--- lib/memory/src/memory/memory-block.h
+++ lib/memory/src/memory/memory-block.h
@@ -23,27 +23,29 @@ public:
   }
 
 public:
-  void used() { _status = MEM_USED | MEM_LOCK; }
-  void used(uint16_t link_to_ptr)
+  void used() { _status = MEM_USED | MEM_FIXED; }
+  void free() { _status = MEM_FREE; }
+  void set_link(uint16_t link_to_ptr)
   {
     _link_to_ptr = link_to_ptr;
     *(uint16_t *)link_to_ptr = _start;
     _status = MEM_USED;
   }
-  void free()
+  void set_start(uint16_t start)
   {
-    if (!(_status & MEM_FIXED)) *(uint16_t *)_link_to_ptr = 0;
-    _status = MEM_FREE;
+    if (is_locked()) return;
+    _start = start;
+    if (_status & MEM_USED)
+      *(uint16_t *)_link_to_ptr = start;
   }
-
-  void set_start(uint16_t start) { _start = start; }
   void set_size(uint16_t size) { _size = size; }
 
-  uint16_t get_link() { return _link_to_ptr; }
   uint16_t get_start() { return _start; }
   uint16_t get_size() { return _size; }
 
   bool is_locked() { return _status & (MEM_LOCK | MEM_FIXED); }
+  bool is_link(uint16_t link_to_ptr) { return link_to_ptr == _link_to_ptr; }
+
   void lock() { _status |= MEM_LOCK; }
   void unlock() { _status &= ~MEM_LOCK; }
   void fixed() { _status |= MEM_FIXED; }
